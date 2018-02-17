6f10238cc71114186da8fdced7930c7c721730df
cymon1997 <cybermonster82@yahoo.com>
2018-02-17 23:21:35 +0700

    improve responsive navbar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,20 +1,24 @@
 <div class="navbar-collection shadow-bottom">
     <div class="nav-top">
         <nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container py-4">
-                <div class="col-md-1 col-sm-2">
-                    <img src="{{ asset('img/logo_transparent.png') }}" width="auto" height="80" class="d-inline-block align-top" alt="logo">
-                </div>
-                <div class="col-md-8 col-sm-10">
-                    <h4 class="navbar-brand" href="#">SMA NEGERI 3 JAKARTA</h4>
-                    <h5 style="color: white;">"Excellent Carrer Begins with Excellent Education"</h5>
-                </div>
-                <div class="col-md-3 col-sm-12 d-none d-md-block">
-                    <a href="http://facebook.com" target="_blank">Facebook</a>
-                    <span style="color: white" class="mx-3">|</span>
-                    <a href="http://twitter.com" target="_blank">Twitter</a>
-                    <span style="color: white" class="mx-3">|</span>
-                    <a href="http://instagram.com" target="_blank">Instagram</a>
+            <div class="container">
+                <div class="container row">
+                    <div class="col-lg-8 col-md-12 col-sm-12 row">
+                        <div class="col-xs">
+                            <img src="{{ asset('img/logo_transparent.png') }}" width="auto" height="80" class="d-inline-block align-top" alt="logo">
+                        </div>
+                        <div class="col">
+                            <h4 class="navbar-brand" href="#">SMA NEGERI 3 JAKARTA</h4>
+                            <h5 style="color: white;">"Excellent Carrer Begins with Excellent Education"</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 d-none d-md-none d-lg-block">
+                        <a href="http://facebook.com" target="_blank">Facebook</a>
+                        <span style="color: white" class="mx-3">|</span>
+                        <a href="http://twitter.com" target="_blank">Twitter</a>
+                        <span style="color: white" class="mx-3">|</span>
+                        <a href="http://instagram.com" target="_blank">Instagram</a>
+                    </div>
                 </div>
             </div>
         </nav>
@@ -28,156 +32,158 @@
                     MENU
                 </button>
                 <!-- Toggleable -->
-                <ul class="navbar-nav">
+                <div class="collapse navbar-collapse" id="navbarDashboard">
+                    <ul class="navbar-nav">
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                  </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
 
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                      Profil
-                    </a>
-                    <div class="dropdown-menu">
-                      <div class="dropdown-submenu dropdown-item">Profil
-                        <!-- <i class="float-right fas fa-angle-right"></i> -->
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Visi Misi</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Sejarah</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Lambang SMAN 3 Jakarta</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Identitas Sekolah</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="dropdown-submenu dropdown-item">Sarana dan Prasarana
-                        <!-- <i class="float-right fas fa-angle-right"></i> -->
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Fasilitas</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Perpustakaan</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Akses dan Lingkungan</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="dropdown-submenu dropdown-item">Manajemen dan Staf
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Struktur Organisasi</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Sambutan Kepsek</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <a class="dropdown-item" href="">Guru</a>
-                    </div>
-                  </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                Profil
+                            </a>
+                            <div class="dropdown-menu">
+                                <div class="dropdown-submenu dropdown-item">Profil
+                                    <!-- <i class="float-right fas fa-angle-right"></i> -->
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Visi Misi</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Sejarah</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Lambang SMAN 3 Jakarta</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Identitas Sekolah</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="dropdown-submenu dropdown-item">Sarana dan Prasarana
+                                    <!-- <i class="float-right fas fa-angle-right"></i> -->
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Fasilitas</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Perpustakaan</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Akses dan Lingkungan</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="dropdown-submenu dropdown-item">Manajemen dan Staf
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Struktur Organisasi</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Sambutan Kepsek</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <a class="dropdown-item" href="">Guru</a>
+                            </div>
+                        </li>
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">PPDB</a>
-                  </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PPDB</a>
+                        </li>
 
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                      Akademik
-                    </a>
-                    <div class="dropdown-menu">
-                      <a class="dropdown-item" href="#">Kalender Akademik</a>
-                      <a class="dropdown-item" href="#">MIPA</a>
-                      <a class="dropdown-item" href="#">IPS</a>
-                    </div>
-                  </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                Akademik
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Kalender Akademik</a>
+                                <a class="dropdown-item" href="#">MIPA</a>
+                                <a class="dropdown-item" href="#">IPS</a>
+                            </div>
+                        </li>
 
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                      Bidang
-                    </a>
-                    <div class="dropdown-menu">
-                      <div class="dropdown-submenu dropdown-item">Kurikulum
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Manajemen Kurikulum</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Seputar Kurikulum</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="dropdown-submenu dropdown-item">Kesiswaan
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Manajemen Kesiswaan</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Seputar Kesiswaan</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="dropdown-submenu dropdown-item">Humas
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Manajemen Humas</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Seputar Humas</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="dropdown-submenu dropdown-item">Sarana dan Prasarana
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Manajemen Sarana dan Prasarana</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Seputar Sarana dan Prasarana</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="dropdown-submenu dropdown-item">Ketatausahaan
-                        <ul class="dropdown-menu">
-                          <li>
-                            <a class="dropdown-item" href="#">Manajemen Ketatausahaan</a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">Seputar Ketatausahaan</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                Bidang
+                            </a>
+                            <div class="dropdown-menu">
+                                <div class="dropdown-submenu dropdown-item">Kurikulum
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Manajemen Kurikulum</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Seputar Kurikulum</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="dropdown-submenu dropdown-item">Kesiswaan
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Manajemen Kesiswaan</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Seputar Kesiswaan</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="dropdown-submenu dropdown-item">Humas
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Manajemen Humas</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Seputar Humas</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="dropdown-submenu dropdown-item">Sarana dan Prasarana
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Manajemen Sarana dan Prasarana</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Seputar Sarana dan Prasarana</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="dropdown-submenu dropdown-item">Ketatausahaan
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Manajemen Ketatausahaan</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Seputar Ketatausahaan</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Berita dan Acara</a>
-                  </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Berita dan Acara</a>
+                        </li>
 
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                      Prestasi
-                    </a>
-                    <div class="dropdown-menu">
-                      <a class="dropdown-item" href="#">Akademik</a>
-                      <a class="dropdown-item" href="#">Olahraga</a>
-                      <a class="dropdown-item" href="#">Seni</a>
-                      <a class="dropdown-item" href="#">Lainnya</a>
-                    </div>
-                  </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                Prestasi
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Akademik</a>
+                                <a class="dropdown-item" href="#">Olahraga</a>
+                                <a class="dropdown-item" href="#">Seni</a>
+                                <a class="dropdown-item" href="#">Lainnya</a>
+                            </div>
+                        </li>
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Alumni</a>
-                  </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Alumni</a>
+                        </li>
 
-                </ul>
+                    </ul>
+                </div>
             </div>
         </nav>
     </div>
